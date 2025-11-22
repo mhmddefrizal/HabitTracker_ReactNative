445fc3fa930b228325d9db2e2927829fd93bba50
okawindanasaputra <okwan2004@gmail.com>
2025-11-22 23:58:08 +0700

    menambahkan style content

--- mobile/app/(tabs)/auth.tsx
+++ mobile/app/(tabs)/auth.tsx
@@ -43,5 +43,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#f5f5f5",
+    },
+    content: {
+        flex: 1,
+        padding: 16,
+        justifyContent: "center",
     }
 });
