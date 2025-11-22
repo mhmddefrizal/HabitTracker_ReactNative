c7a37cbe6ad0e9e476f939879caf635365a2e012
okawindanasaputra <okwan2004@gmail.com>
2025-11-22 23:46:58 +0700

    menambahkan fungsi button 2

--- mobile/app/(tabs)/auth.tsx
+++ mobile/app/(tabs)/auth.tsx
@@ -2,16 +2,33 @@ import { KeyboardAvoidingView, Platform, View } from "react-native";
 import { Text, TextInput } from "react-native-paper";
 
 export default function AuthScreen() {
-    const [isSignUp, setIsSignUp] = useState<Boolean>(false);
-    return <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"}>
-        <View>
-            <Text>{isSignUp ? "Buat Akun" : "selamat datang"}</Text>
-            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="emailkamu@gmail.com" mode="outlined" ></TextInput>
-            <TextInput label="Password" autoCapitalize="none" keyboardType="email-address" mode="outlined" ></TextInput>
+  const [isSignUp, setIsSignUp] = useState<Boolean>(false);
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "android" ? "padding" : "height"}
+    >
+      <View>
+        <Text>{isSignUp ? "Buat Akun" : "selamat datang"}</Text>
+        <TextInput
+          label="Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="emailkamu@gmail.com"
+          mode="outlined"
+        ></TextInput>
+        <TextInput
+          label="Password"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          mode="outlined"
+        ></TextInput>
 
-            <button mode="contained">{isSignUp ? "Buat Akun" : "login"}</button>
+        <button mode="contained">{isSignUp ? "Buat Akun" : "login"}</button>
 
-            <button mode="text">Sudah punya akun?</button>
-        </View>
+        <button mode="text">
+          {isSignUp ? "Sudah punya akun? login" : "belum punya akun? buat akun"}
+        </button>
+      </View>
     </KeyboardAvoidingView>
+  );
 }
